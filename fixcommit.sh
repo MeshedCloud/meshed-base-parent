54f14207b2fd1836e5f62aa8ab40336b9ff35a57
Vincent Vic <vic.coding@gmail.com>
2023-02-24 16:32:50 +0800

    add spring-context version

---
+++ fixcommit.sh
@@ -0,0 +1,38 @@
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="hougq@homeking365.com"
+CORRECT_NAME="Vincent Vic"
+CORRECT_EMAIL="vic.coding@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+# OLD_EMAIL 为历史邮箱，无需name，用邮箱搜索内容
+# CORRECT_NAME,CORRECT_EMAIL 为希望修改后提交人和提交邮箱
+# 执行完成用git log查看就变了
+
+git filter-branch --env-filter '
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
